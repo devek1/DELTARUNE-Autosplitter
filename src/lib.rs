4d44116c43b0e0dd12f0873c4a6bf4774ca98a63
devek1 <devek1001@gmail.com>
2026-07-19 21:02:17 +0300

    make the name checks stricter

--- src/lib.rs
+++ src/lib.rs
@@ -279,7 +279,7 @@ async fn main() {
                         if name == "@@ObjectContainer@@" {
                             break;
                         }
-                        if name != "" && name.is_ascii() {
+                        if name != "" && name.chars().all(|x| x.is_ascii_alphanumeric() || x == '@' || x == '_' || x == '-') {
                             stringsList.insert(i + match version {GMS2_v2_2_0=>0,_=>100000}, name.to_string());
 
                             if matches!(name,"plot"|"mystring"|"flag"|"item"|"litem") {
@@ -303,7 +303,7 @@ async fn main() {
                             if name == "@@ObjectContainer@@" {
                                 break;
                             }
-                            if name != "" && name.is_ascii() {
+                            if name != "" && name.chars().all(|x| x.is_ascii_alphanumeric() || x == '@' || x == '_') {
                                 stringsList.insert(i, name.to_string());
 
                                 if matches!(name,"plot"|"mystring"|"flag"|"item"|"litem") {
@@ -358,7 +358,7 @@ async fn main() {
                             if objAddr.is_null() { break; }
                             let _name = process.read_pointer_path::<ArrayCString<64>>(objAddr,ps,&[objPropOff,match version { GMS2_v2_2_0 => 0x14, _ => 0 },0x0]).unwrap_or_default();
                             let name = _name.validate_utf8().unwrap_or_default();
-                            if name != "" && name.is_ascii() {
+                            if name != "" && name.chars().all(|x| x.is_ascii_alphanumeric() || x == '@' || x == '_') {
                                 /*if matches!(name,"obj_writer"|"obj_moneydisplay"|"DEVICE_NAMER"|"obj_berdly_smoke") {
                                     asr::print_message(format!("{} found at index {} layer {}, address {}",name,i,_layer,objAddr).as_str());
                                 }*/
